ff73bdb755531c2671e58a342aebd2691edcaef6
DELL <DELL@DESKTOP-F4E7MDH>
2022-12-17 17:05:00 +0530

    Commit fir review-tc_215

---
+++ src/main/java/organizationTest/CreateOrgTest.java
@@ -0,0 +1,14 @@
+package organizationTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest 
+{
+   @Test
+    public void tc_215_CreateOrgWithMandatoryFieldsTest()
+    {
+	   System.out.println("TC_251");
+    }
+   
+   
+}
